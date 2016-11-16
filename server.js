e5e67329e0b2f8921e19078a5552432a88cf54b4
Jesus Gonzalez <jesus.gonzalez.brittnau@outlook.com>
2016-11-16 18:29:13 +0100

    Abgeschlossenen Kur sichern in Github

--- server.js
+++ server.js
@@ -2,16 +2,32 @@
 require('dotenv').config();
 
 // grab our dependencies
-const express      = require('express');
-    app            = express();
-    port           = process.env.PORT||8080;
-    dbHost         = process.env.DB_URI;
-    expressLayouts = require('express-ejs-layouts');
-    mongoose       = require('mongoose');
-    bodyParser     = require('body-parser');
+const express        = require('express');
+    app              = express();
+    port             = process.env.PORT||8080;
+    dbHost           = process.env.DB_URI;
+    expressLayouts   = require('express-ejs-layouts');
+    mongoose         = require('mongoose');
+    bodyParser       = require('body-parser');
+    session          = require('express-session');
+    cookieParser     = require('cookie-parser');
+    flash            = require('connect-flash');
+    expressValidator = require('express-validator');
+
 
 
 // configure our application =====================
+
+//set session and cookie parser
+app.use(cookieParser());
+app.use(session({
+    secret: process.env.SECRET,
+    cookie: {maxAge: 60000},
+    resave: false, // forces the session to be saved back to the store
+    saveUninitialized: false // don't save unmodified sessions
+}));
+app.use(flash());
+
 //tell express where to look for static assets
 app.use(express.static(__dirname+'/public'));
 
@@ -24,6 +40,7 @@ mongoose.connect(dbHost);
 
 // use boy-parser to grab info from a form
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator())
 
 // set the routes =====================
 app.use(require('./app/routes'));
