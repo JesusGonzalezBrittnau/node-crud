38f9667cb43a614bf457df887910e85999426c8d
Jesus Gonzalez <jesus.gonzalez.brittnau@outlook.com>
2018-04-09 21:59:34 +0200

    dummy commit for openshift

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -1,4 +1,4 @@
-//tell express where to look for static assets
+//tell express where to look for static assets (test)
 module.exports = {
     showHome: (req, res) => {
         res.render('pages/home');
